272b368fa5fec0bd7c819724fb9455d79df8cff6
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2023-11-10 13:59:34 +0100

    added visualitation of the paths in the visuaGraph.py. it now contains arrows that show wich ways you can go

--- packages/my_packages/src/visuaGraph.py
+++ packages/my_packages/src/visuaGraph.py
@@ -1,47 +1,13 @@
-from PIL import Image
+import matplotlib.pyplot as plt
+import networkx as nx
 
-def create_image_with_path(graph, path, cell_size=20):
-    min_x, min_y, max_x, max_y = float('inf'), float('inf'), 0, 0
-
-    for node in graph:
-        min_x = min(min_x, node[0])
-        min_y = min(min_y, node[1])
-        max_x = max(max_x, node[0])
-        max_y = max(max_y, node[1])
-
-    width = max_x - min_x + 1
-    height = max_y - min_y + 1
-
-    image = Image.new("RGB", (width * cell_size, height * cell_size), "white")
-    pixels = image.load()
-
-    for node in graph:
-        x = node[0] - min_x
-        y = node[1] - min_y
-
-        # Check if the current node is in the path
-        if node in path:
-            color = (0, 255, 0)  # green for the path
-        else:
-            # Check if the current node is a wall
-            if not graph[node]:
-                color = (0, 0, 0)  # black for walls
-            else:
-                color = (255, 255, 255)  # white for open paths
-
-        for dy in range(cell_size):
-            for dx in range(cell_size):
-                pixels[x * cell_size + dx, y * cell_size + dy] = color
-
-    image.show()
-
-# Your graph
+# Your graph and start/goal nodes
 graph = {
-    (0, 0): {(0, 1): 0, (1, 0): 1},
-    (0, 1): {(0, 0): 0, (0, 2): 1, (1, 1): 1},
-    (0, 2): {(0, 1): 0, (0, 3): 1, (1, 2): 1},
-    (0, 3): {(0, 2): 0, (0, 4): 1, (1, 3): 1},
-    (0, 4): {(0, 3): 0, (1, 4): 1},
+    (0, 0): {(0, 1): 1, (1, 0): 1},
+    (0, 1): {(0, 0): 1, (0, 2): 1, (1, 1): 1},
+    (0, 2): {(0, 1): 1, (0, 3): 1, (1, 2): 1},
+    (0, 3): {(0, 2): 1, (0, 4): 1, (1, 3): 1},
+    (0, 4): {(0, 3): 1, (1, 4): 1},
 
     (1, 0): {(0, 0): 1, (1, 1): 1, (2, 0): 1},
     (1, 1): {(0, 1): 1, (1, 0): 1, (1, 2): 1, (2, 1): 1},
@@ -49,27 +15,57 @@ graph = {
     (1, 3): {(0, 3): 1, (1, 2): 1, (1, 4): 1, (2, 3): 1},
     (1, 4): {(0, 4): 1, (1, 3): 1, (2, 4): 1},
 
-    (2, 0): {(1, 0): 0, (2, 1): 1, (3, 0): 0},
-    (2, 1): {(1, 1): 0, (2, 0): 1, (2, 2): 0, (3, 1): 1},
-    (2, 2): {(1, 2): 0, (2, 1): 1, (2, 3): 0, (3, 2): 1},
-    (2, 3): {(1, 3): 0, (2, 2): 1, (2, 4): 0, (3, 3): 1},
-    (2, 4): {(1, 4): 0, (2, 3): 1, (3, 4): 0},
+    (2, 0): {(1, 0): 1, (2, 1): 1, (3, 0): 1},
+    (2, 1): {(1, 1): 1, (2, 0): 1, (2, 2): 1, (3, 1): 1},
+    (2, 2): {(1, 2): 1, (2, 1): 1, (2, 3): 1, (3, 2): 1},
+    (2, 3): {(1, 3): 1, (2, 2): 1, (2, 4): 1, (3, 3): 1},
+    (2, 4): {(1, 4): 1, (2, 3): 1, (3, 4): 1},
 
-    (3, 0): {(2, 0): 1, (3, 1): 1, (4, 0): 0},
-    (3, 1): {(2, 1): 1, (3, 0): 1, (3, 2): 0, (4, 1): 1},
-    (3, 2): {(2, 2): 1, (3, 1): 1, (3, 3): 0, (4, 2): 1},
-    (3, 3): {(2, 3): 1, (3, 2): 1, (3, 4): 0, (4, 3): 1},
-    (3, 4): {(2, 4): 1, (3, 3): 1, (4, 4): 0},
+    (3, 0): {(2, 0): 1, (3, 1): 1, (4, 0): 1},
+    (3, 1): {(2, 1): 1, (3, 0): 1, (3, 2): 1, (4, 1): 1},
+    (3, 2): {(2, 2): 1, (3, 1): 1, (3, 3): 1, (4, 2): 1},
+    (3, 3): {(2, 3): 1, (3, 2): 1, (3, 4): 1, (4, 3): 1},
+    (3, 4): {(2, 4): 1, (3, 3): 1, (4, 4): 1},
 
-    (4, 0): {(3, 0): 0, (4, 1): 1},
-    (4, 1): {(3, 1): 0, (4, 0): 1, (4, 2): 1},
-    (4, 2): {(3, 2): 0, (4, 1): 1, (4, 3): 1},
-    (4, 3): {(3, 3): 0, (4, 2): 1, (4, 4): 1},
-    (4, 4): {(3, 4): 0, (4, 3): 1}
+    (4, 0): {(3, 0): 1, (4, 1): 1},
+    (4, 1): {(3, 1): 1, (4, 0): 1, (4, 2): 1},
+    (4, 2): {(3, 2): 1, (4, 1): 1, (4, 3): 1},
+    (4, 3): {(3, 3): 1, (4, 2): 1, (4, 4): 1},
+    (4, 4): {(3, 4): 1, (4, 3): 1}
 }
 
-# Example start and goal nodes
-path = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+start_node = (0, 0)
+goal_node = (4, 4)
+path = [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (1, 4), (2, 4), (3, 4), (4, 4)]
 
-# Example usage of the create_image_with_path function
-create_image_with_path(graph, path)
+# Filter the path to include only nodes that are in the graph
+filtered_path = [node for node in path if node in graph]
+
+# Create a NetworkX DiGraph (Directed Graph)
+G = nx.DiGraph()
+
+for node, neighbors in graph.items():
+    G.add_node(node)
+    for neighbor in neighbors:
+        G.add_edge(node, neighbor)
+
+# Draw the graph
+pos = {node: (node[1], -node[0]) for node in G.nodes()}  # Adjust coordinates for visualization
+
+plt.figure(figsize=(8, 8))
+nx.draw_networkx(G, pos, with_labels=False, node_color='white', node_size=500, font_size=10, font_color='black')
+
+# Mark the start and goal nodes
+start_color = 'green'
+goal_color = 'green'
+node_colors = ['black' if node not in (start_node, goal_node) else start_color if node == start_node else goal_color for node in G.nodes()]
+nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=500)
+
+# Draw the path with arrowheads
+path_edges = [(filtered_path[i], filtered_path[i + 1]) for i in range(len(filtered_path) - 1)]
+path_colors = ['green' for _ in path_edges]
+nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='green', width=2, arrowsize=20)
+
+
+plt.axis('off')
+plt.show()
